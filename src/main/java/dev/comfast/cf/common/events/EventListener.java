4ab172e840b0af70c70d3065f44e2130b5e24b1e
Piotr Kluz <piox89@gmail.com>
2023-06-11 15:03:43 +0200

    Remove events (they are in comfast-commons)

--- src/main/java/dev/comfast/cf/common/events/EventListener.java
+++
@@ -1,5 +0,0 @@
-package dev.comfast.cf.common.events;
-public interface EventListener<LogContext> {
-    default void before(BeforeEvent<LogContext> event) {}
-    default void after(AfterEvent<LogContext> event) {}
-}
--- src/main/java/dev/comfast/cf/common/events/EventStatus.java
+++
@@ -1,2 +0,0 @@
-package dev.comfast.cf.common.events;
-public enum EventStatus {IN_PROGRESS, PASSED, FAILED}
